41f25e7b9c80070da10910491286a7fdc9d3dfb9
automacaopostcmb <automacaopostcmb@gmail.com>
2026-04-06 14:16:01 -0300

    Update app-forum.js

--- app-forum.js
+++ app-forum.js
@@ -87,7 +87,7 @@ function formatarDataForum(value) {
 
   const horaStr = String(horas).padStart(2, '0');
 
-  return `Mensagem enviada em ${dia}/${mes}/${ano} - ${horaStr}:${minutos}${ampm}`;
+  return `${dia}/${mes}/${ano} - ${horaStr}:${minutos}${ampm}`;
 }
 
 function statusClass(status) {
