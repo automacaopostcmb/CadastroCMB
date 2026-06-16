0fb82bdbef5e61dbdc179fbf6d1a38e8da4192b4
automacaopostcmb <automacaopostcmb@gmail.com>
2026-06-16 00:53:09 -0300

    Update app-expo_artistas.js

--- app-expo_artistas.js
+++ app-expo_artistas.js
@@ -1,7 +1,7 @@
 /* =========================================================
    CONFIG – ARTISTAS (IDs, URLs e moldes)
    ========================================================= */
-const FRAME_URL = 'https://cdn.jsdelivr.net/gh/automacaopostcmb-bit/CadastroCMB@main/assets/areaartista3.png';
+const FRAME_URL = 'https://cdn.jsdelivr.net/gh/automacaopostcmb-bit/CadastroCMB@main/assets/areaartista4.png';
 const AUTH_URL = "https://script.google.com/macros/s/AKfycbyMbkkFdzYC_BfMsi5WKW6xbOKdjbNbW635vovOLYHGXdso2S_1a2Wdfvur790y0BM46g/exec";
 const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyAIRNSN5yaoSIKzxgf5rnme1ryxveWHmePMC6qRDtrkso3pZtQ-7iMW4pi94LbW1uS/exec";
 
